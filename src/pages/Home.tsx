1f09c551a6cf5c1d821ff42ec4f27f34237cc2e8
Andray G <gorbanpixels@gmail.com>
2026-01-27 17:10:49 -0500

    Update site changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import Hero from '../components/Hero'
 import About from '../components/About'
-import Services from '../components/Services'
 import Blog from '../components/Blog'
 import FAQ from '../components/FAQ'
 
